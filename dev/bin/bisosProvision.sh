f4418f1cf5da35a368632da3470829638a44bb4c
LSIP User <lsipusr@intra-Latitude-7390>
2020-06-11 22:37:53 -0700

    initial basic features.

--- dev/bin/bisosProvision.sh
+++ dev/bin/bisosProvision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IcmBriefDescription="NOTYET: Short Description Of The Module"
+IcmBriefDescription="BISOS Provisioning -- Create the needed framework for BISOS."
 
 ####+BEGINNOT: bx:dblock:global:file-insert :file "/opt/idaas/gitRepos/idaas/idaas/tools/common/lib/bash/mainRepoRootDetermine.bash"
 #
@@ -40,18 +40,21 @@ fi
 
 #mainRepoRoot=$( cd $(dirname $0); git rev-parse --show-toplevel 2> /dev/null )
 
+rootBase="/opt"
+beSilent="false"
+
+provisionerBase="${rootBase}/bisosProvisioner"
+      
+genesisGitRepoCloneCmnd="git clone https://github.com/bxGenesis/provisioners.git"
+
 
 function vis_describe {  cat  << _EOF_
-Module description comes here.
+BISOS Provisioer is a minimal standaloneIcm that creates a standaloneIcmEnv
+and invokes facilities there.
 _EOF_
 		      }
 
 # Import Libraries
-
-#
-#. ${opLibBase}/pidLib.sh
-# # /opt/public/osmt/lib/portLib.sh
-#. ${opLibBase}/portLib.sh
 
 
 function G_postParamHook {
@@ -68,10 +71,20 @@ function vis_examples {
     visLibExamplesOutput ${G_myName} 
   cat  << _EOF_
 $( examplesSeperatorTopLabel "${G_myName}" )
-$( examplesSeperatorChapter "Chapter Title" )
-$( examplesSeperatorSection "Section Title" )
-${G_myName} ${extraInfo} -i someAction # Does Some Work
-${G_myName} ${extraInfo} -p someParam=someValue -i someAction arg1 arg2 "And The Third One"
+$( examplesSeperatorChapter "BISOS Provisioning" )
+$( examplesSeperatorSection "Create bisosProvision base directories" )
+${G_myName} ${extraInfo} -i gitBinsPrep
+${G_myName} ${extraInfo} -i gitPrep
+${G_myName} ${extraInfo} -p rootBase=/opt -i provisionerRepoClone
+${G_myName} ${extraInfo} -p rootBase=/opt -i provisionerBasesPrep
+${G_myName} ${extraInfo} -i provisionerRepoClone
+${G_myName} ${extraInfo} -i provisionerBasesPrep
+$( examplesSeperatorSection "Create Accounts" )
+${G_myName} ${extraInfo} -i updateAccts
+$( examplesSeperatorSection "Create BisosProv Virtenvs" )
+${G_myName} ${extraInfo} -i pythonSysEnvPrepForVirtenvs
+$( examplesSeperatorSection "Create /bisos Bases" )
+${G_myName} ${extraInfo} -i bisosBaseDirsSetup
 _EOF_
 }
 
@@ -79,21 +92,173 @@ noArgsHook() {
   vis_examples
 }
 
-function vis_someAction {
+function modulePrep {
     G_funcEntry
     function describeF {  G_funcEntryShow; cat  << _EOF_
-echo someParam and args 
 _EOF_
     }
-    EH_assert [[ $# -ge 0 ]]
+    EH_assert [[ $# -eq 0 ]]
 
-    lpDo echo "Param: someParam=${someParam}"
+    provisionerBase="${rootBase}/bisosProvisioner"
+    
+    lpReturn
+}
 
-    local each
-    for each in "$@" ; do
-	lpDo echo "Args List: ${each}"
-    done
+
+function vis_gitBinsPrep {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    opDo sudo apt-get update
+    opDo sudo apt-get -y install git      
 
     lpReturn
 }
 
+
+function vis_gitPrep {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    #opDo sudo git init
+
+    lpReturn
+}
+
+
+
+function vis_provisionerRepoClone {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    opDo modulePrep
+
+    local gitReposBase="$provisionerBase}/gitRepos"
+    
+    opDo sudo  mkdir -p "${gitReposBase}"
+
+    inBaseDirDo "${gitReposBase}" sudo git clone https://github.com/bxGenesis/provisioners.git
+    
+    lpReturn
+}
+
+
+function vis_provisionerBasesPrep {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    modulePrep
+
+    # /opt/bisosProvisioner}/gitRepos/provisioners
+    local provisionersBase="$provisionerBase}/gitRepos/provisioners"
+
+    if [ -d "${provisionersBase}" ] ; then
+	if [ "${beSilent}" != "true" ] ; then  
+	    ANT_raw "${provisionersBase} is in place, preparation skipped"
+	fi
+    else	
+    
+	opDo vis_gitBinsPrep
+
+	opDo vis_gitPrep
+
+	opDo vis_provisionerRepoClone
+    fi
+    
+    lpReturn
+}
+
+function provisionersPrep {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    modulePrep
+    
+    beSilent="true"
+    
+    lpReturn
+}
+
+
+function vis_updateAccts {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    opDo provisionersPrep
+
+    # /opt/bisosProvisioner}/gitRepos/provisioners/bin/bisosAccounts.sh
+    local bisosAccountsProg="$provisionerBase}/gitRepos/provisioners/bin/bisosAccounts.sh"
+    opDo vis_provisionerBasesPrep    
+
+    if [ ! -x "${bisosAccountsProg}" ] ; then
+	EH_problem "Missing ${bisosAccountsProg}"
+	lpReturn 1
+    else	
+    	opDo "${bisosAccountsProg}" -h -v -n showRun -i fullUpdate passwd_tmpSame
+    fi
+    
+    lpReturn
+}
+
+function vis_pythonSysEnvPrepForVirtenvs {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    opDo provisionersPrep
+
+    # /opt/bisosProvisioner}/gitRepos/provisioners/bin/bisosBaseDirsSetup.sh
+    local bisosProg="$provisionerBase}/gitRepos/provisioners/bin/bisosBaseDirsSetup.sh"
+
+    if [ ! -x "${bisosProg}" ] ; then
+	EH_problem "Missing ${bisosProg}"
+	lpReturn 1
+    else	
+    	opDo "${bisosProg}" -h -v -n showRun -i pythonSysEnvPrepForVirtenvs
+    fi
+    
+    lpReturn
+}
+
+
+function vis_bisosBaseDirsSetup {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    opDo provisionersPrep
+
+    # /opt/bisosProvisioner}/gitRepos/provisioners/bin/bisosBaseDirsSetup.sh
+    local bisosProg="$provisionerBase}/gitRepos/provisioners/bin/bisosBaseDirsSetup.sh"
+
+    if [ ! -x "${bisosProg}" ] ; then
+	EH_problem "Missing ${bisosProg}"
+	lpReturn 1
+    else	
+    	opDo "${bisosProg}" -h -v -n showRun -i bisosBaseDirsSetup
+    fi
+    
+    lpReturn
+}
